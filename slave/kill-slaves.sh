fc16f8b1da86d671a7d10c26d9aea7405c94a627
Bryan Robbins <bryantrobbins@gmail.com>
2015-09-27 22:56:04 -0400

    Make image name configurable

--- slave/kill-slaves.sh
+++ slave/kill-slaves.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Args
+image=$1
+
 # Stop existing slaves
-docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker stop
-docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker rm
+docker ps -a | awk '{print $1,$2}' | grep ${image} | awk '{print $1}' | xargs docker stop
+docker ps -a | awk '{print $1,$2}' | grep ${image} | awk '{print $1}' | xargs docker rm
 
--- slave/start-slaves.sh
+++ slave/start-slaves.sh
@@ -3,12 +3,13 @@
 master_host=$1
 master_user=$2
 master_pass=$3
-nslave=$4
+image=$4
+nslave=$5
 
 # Start some slaves
 for i in `seq 1 $nslave`;
 do
   echo $i
-  docker run -d --dns 128.8.127.50 -P --name slave-$i -e SWARM_MASTER=$master_host -e SWARM_USER=$master_user -e SWARM_PASSWORD=$master_pass bryantrobbins/jslave
+  docker run -d --dns 128.8.127.50 -P --name slave-$i -e SWARM_MASTER=$master_host -e SWARM_USER=$master_user -e SWARM_PASSWORD=$master_pass ${image}
   sleep 10
 done
