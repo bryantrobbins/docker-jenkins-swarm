7be556e489caed7c737efb7a574e0e2b8a814a33
Bryan Robbins <bryantrobbins@gmail.com>
2015-11-10 13:11:03 -0500

    Add proper name to jslave-r build script

--- jslave-r/build.sh
+++ jslave-r/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build --no-cache -t="bryantrobbins/jslave" .
+docker build --no-cache -t="bryantrobbins/jslave-r" .
 if [ $? -eq 0 ]
 	then
 		docker save -o jslave.tar bryantrobbins/jslave-r
