1a50ad332da4ca231e8548ac6ed4cc30f9779786
Bryan T Robbins <Bryan.Robbins@finra.org>
2015-11-20 09:53:18 -0500

    Add new R packages used by lasso

--- jslave-r/install.r
+++ jslave-r/install.r
@@ -5,6 +5,8 @@ options(echo=TRUE)
 cran <- "http://cran.rstudio.com/"
 install.packages("e1071", repos=cran)
 install.packages("glmnet", repos=cran)
+install.packages("methods", repos=cran)
+install.packages("mda", repos=cran)
 install.packages("rmongodb", repos=cran)
 install.packages("ROCR", repos=cran)
 install.packages("devtools", repos=cran)
