247dc416f1c1b153d97acb8c9585386fe537275c
Bryan Robbins <bryantrobbins@gmail.com>
2015-11-21 09:53:01 -0500

    Add more packages to r slave container

--- jslave-r/install.r
+++ jslave-r/install.r
@@ -3,9 +3,10 @@ options(echo=TRUE)
 
 # Needed for portable installs
 cran <- "http://cran.rstudio.com/"
-install.packages("e1071", repos=cran)
 install.packages("glmnet", repos=cran)
 install.packages("rmongodb", repos=cran)
+install.packages("data.table", repos=cran)
+install.packages("mda", repos=cran)
 install.packages("ROCR", repos=cran)
 install.packages("devtools", repos=cran)
 require(devtools)
